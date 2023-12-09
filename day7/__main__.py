ac7764e28d611cc7cc870248828dc8483441c2a9
selena <appleselena@gmail.com>
2023-12-09 00:23:19 -0500

    day7

--- day7/__main__.py
+++ day7/__main__.py
@@ -6,18 +6,18 @@ def main():
     part2()
 
 class Hand:
-    def __init__(self, cards, bid, hand_type):
+    def __init__(self, cards, bid, card_type):
         self.cards = cards
         self.bid = bid
-        self.hand_type = hand_type
+        self.card_type = card_type
 
     def __lt__(self, other):
-        if self.hand_type == other.hand_type:
+        if self.card_type == other.card_type:
             for card, other_card in zip(self.cards, other.cards):
                 if card != other_card:
                     return card < other_card
             return True
-        return self.hand_type < other.hand_type
+        return self.card_type < other.card_type
 
 def part1():
     card_list = []
@@ -49,7 +49,42 @@ def part1():
     print("Part 1:", score)
 
 def part2():
-    pass
+    card_list = []
+    letter_map = {
+        "T": "B",
+        "J": "1",
+        "K": "R",
+        "A": "Z",
+    }
+
+    with open("day7/input.txt", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            [cards, bid] = line.split(" ")
+            cards = cards.strip()
+            bid = int(bid.strip())
+
+            for old, new in letter_map.items():
+                cards = cards.replace(old, new)
+                
+            counter = Counter(cards)
+            joker_count = counter["1"]
+            del counter["1"]
+            counter_list = list(counter.most_common())
+            card_type = (5 - len(counter_list))
+            if card_type < 5:
+                card_type += counter_list[0][1] + joker_count
+            else:
+                card_type += 4
+
+            current_hand = Hand(cards, bid, card_type)
+            bisect.insort_right(card_list, current_hand)
+
+    score = 0
+    for i, card in enumerate(card_list):
+        score += (i + 1) * card.bid
+        
+    print("Part 2:", score)
 
 if __name__ == "__main__":
     main()
